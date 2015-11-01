d73fe2ed9e32c18ed663f8b2d4b24593758d620b
Jan Dufek <jan@dufek.me>
2015-11-01 22:27:12 +0000

    Draft of basic layout

---
+++ layout.php
@@ -0,0 +1,18 @@
+<div id="wrapper">
+    <div id="header">
+        <div id="header-content">
+            Header
+        </div>
+    </div>
+    <div id="content">
+        <div id="sidebar">
+            Sidebar<br/>
+            Sidebar<br/>
+            Sidebar<br/>
+        </div>
+        <div id="main">
+            <div id="map"></div>
+        </div>
+    </div>
+    <div class="push"></div>
+</div>
