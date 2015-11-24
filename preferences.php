b8939c1985ab883691b9a68e3d47925eb0c4343a
ohnarya <ohnarya@gmail.com>
2015-11-24 14:21:01 -0600

    Delete preferences.php

--- preferences.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$DATABASE_HOST = "mongodb://jandufek-project634php-2006737:27017";
-
-$m  = new MongoClient($DATABASE_HOST);  /*connect*/
-$db = $m->selectDB("project634");  /*select DB*/
-
-?>
